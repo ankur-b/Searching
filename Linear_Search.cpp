48cf5998d4efd0e13710fe88cd8944f55b18eb9f
resistance29 <31529543+resistance29@users.noreply.github.com>
2018-02-01 22:39:37 +0530

    Create Linear_Search.cpp

---
+++ Linear_Search.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int i, num, n, c = 0, pos;
+  cout << "Enter the array size : ";
+  cin >> n;
+  int arr[n];
+  cout << "Enter Array Elements : ";
+  for (i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  cout << "Enter the number to be searched : ";
+  cin >> num;
+  for (i = 0; i < n; i++) {
+    if (arr[i] == num) {
+      c = 1;
+      pos = i + 1;
+      break;
+    }
+  }
+  if (c == 0) {
+    cout <<"Number not found..!!";
+  } else {
+    cout <<"found at position " << pos;
+  }
+}
+
+
+
+
+
